d863eda7f9590d3dfe6ac104e63efc98db524d53
oreols <oreak1500@gmail.com>
2023-10-20 14:05:17 +0200

    przycisk rozpocznij prowadzi uzytkownika do MapActivity.java

--- app/src/main/java/com/oreo7/mapamiejsc/MainActivity.java
+++ app/src/main/java/com/oreo7/mapamiejsc/MainActivity.java
@@ -2,13 +2,30 @@ package com.oreo7.mapamiejsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMapActivity();
+            }
+        });
+    }
+
+    public void openMapActivity(){
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
