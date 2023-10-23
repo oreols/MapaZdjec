2dd7f43e4f255e0897ab3852182a781ea2461ba4
oreols <oreak1500@gmail.com>
2023-10-23 23:43:33 +0200

    nazwa uzytkownika zapisuje sie teraz do stringa

--- app/src/main/java/com/oreo7/mapamiejsc/MainActivity.java
+++ app/src/main/java/com/oreo7/mapamiejsc/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.editTextText);
+        String text = editText.getText().toString();
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/oreo7/mapamiejsc/MapActivity.java
+++ app/src/main/java/com/oreo7/mapamiejsc/MapActivity.java
@@ -1,8 +1,15 @@
 package com.oreo7.mapamiejsc;
 
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.osmdroid.views.MapView;
 
 public class MapActivity extends AppCompatActivity {
 
