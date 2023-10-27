6247babf4c049baec61ff6b725708aee8c66f99e
oreols <oreak1500@gmail.com>
2023-10-27 22:01:27 +0200

    mapa działa poprawnie w MapActivity wraz ze zbliżaniem i oddalaniem + ikonka lokalizacji z kompasem

--- app/src/main/java/com/oreo7/mapamiejsc/MapActivity.java
+++ app/src/main/java/com/oreo7/mapamiejsc/MapActivity.java
@@ -1,21 +1,64 @@
 package com.oreo7.mapamiejsc;
 
-import static java.security.AccessController.getContext;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.config.IConfigurationProvider;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import org.osmdroid.views.MapView;
 
 public class MapActivity extends AppCompatActivity {
+    private MapView mapView;
+    private IMapController mapController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_map);
+
+        Configuration.getInstance().load(this, getSharedPreferences(getPackageName() + "_preferences", MODE_PRIVATE));
+
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG, "Permission is granted");
+            } else {
+                Log.v(TAG, "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
+
+        mapView = findViewById(R.id.mapview);
+        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE); // Ustaw źródło mapy
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+        mapView.setMultiTouchControls(true);
+
+
+        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), mapView);
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.enableFollowLocation();
+        mapView.getOverlays().add(myLocationOverlay);
+
+
+
+        mapController = mapView.getController();
+        mapController.setZoom(15.0); // Ustaw początkowy poziom przybliżenia
     }
 }
