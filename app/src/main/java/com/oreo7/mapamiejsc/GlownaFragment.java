9b14579a66a42f1f97e51d16744987e7b259b90c
oreols <oreak1500@gmail.com>
2023-11-21 17:47:13 +0100

    3 nowe layouty, menu teraz odsyła do tych layoutow + menu zapamiętuje nasz ostatni wybór

---
+++ app/src/main/java/com/oreo7/mapamiejsc/GlownaFragment.java
@@ -0,0 +1,17 @@
+package com.oreo7.mapamiejsc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GlownaFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.glowna, container, false);
+    }
+}
